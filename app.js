8af402489ce2a81025d10c1d8f999812e1fe5337
Tom Vandenbussche <tom.vandenbussche.s7171@student.hogent.be>
2016-11-30 13:06:06 +0100

    BabelJS support

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
-var express = require('express');
-var path = require('path');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+import express from "express";
+import path from "path";
+import logger from "morgan";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import users from "./routes/users";
+import index from "./routes/index";
 
-var index = require('./routes/index');
-var users = require('./routes/users');
-
-var app = express();
+let app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,7 +23,7 @@ app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  let err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
