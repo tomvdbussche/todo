a4ee58edc447b485588c5edea29e636a99b72857
Tom Vandenbussche <tom.vandenbussche.s7171@student.hogent.be>
2016-12-06 00:03:37 +0100

    Separated controller from task-list directive

---
+++ public/app/components/tasklist/taskListController.js
@@ -0,0 +1,31 @@
+angular
+    .module('app.core')
+    .controller('TaskListController', ['$scope', 'ListService', function ($scope) {
+        let updateCount = function () {
+            $scope.list.tasks.completed = $scope.list.tasks.filter(function (task) {
+                return task.completed;
+            }).length;
+        };
+        $scope.toggle = function (task) {
+            console.log('Toggling task ' + task.name);
+            ListService.toggle(task)
+                .then(function () {
+                    updateCount();
+                });
+        };
+        $scope.delete = function (task) {
+            console.log('Removing task ' + task.name);
+            ListService.delete(task);
+        };
+        $scope.addTask = function () {
+            if (!$scope.name || $scope.name === '') {
+                return;
+            } else {
+                console.log('Adding new task ' + $scope.name);
+                ListService.addTask($scope.list, {
+                    name: $scope.name
+                });
+            }
+        };
+        updateCount();
+    }]);
--- public/app/components/tasklist/taskListDirective.js
+++ public/app/components/tasklist/taskListDirective.js
@@ -1,39 +1,11 @@
 angular
     .module('app.core')
-    .directive('taskList', ['ListService', function (ListService) {
+    .directive('taskList', function () {
         return {
             scope: {
                 list: '='
             },
             templateUrl: 'app/components/tasklist/taskListView.html',
-            controller: function ($scope) {
-                let updateCount = function () {
-                    $scope.list.tasks.completed = $scope.list.tasks.filter(function (task) {
-                        return task.completed;
-                    }).length;
-                };
-                $scope.toggle = function (task) {
-                    console.log('Toggling task ' + task.name);
-                    ListService.toggle(task)
-                        .then(function () {
-                            updateCount();
-                        });
-                };
-                $scope.delete = function (task) {
-                    console.log('Removing task ' + task.name);
-                    ListService.delete(task);
-                };
-                $scope.addTask = function () {
-                    if (!$scope.name || $scope.name === '') {
-                        return;
-                    } else {
-                        console.log('Adding new task ' + $scope.name);
-                        ListService.addTask($scope.list, {
-                            name: $scope.name
-                        });
-                    }
-                };
-                updateCount();
-            }
+            controller: 'TaskListController'
         };
-    }]);
+    });
