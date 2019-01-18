8431b0a74dcedab64b427c92d91f2bbd55cadc79
Tom Vandenbussche <tomvdbussche@users.noreply.github.com>
2019-01-19 00:27:41 +0100

    Specify crypto digest

--- models/user.js
+++ models/user.js
@@ -12,11 +12,11 @@ var schema = new Schema({
 
 schema.methods.setPassword = function (password) {
     this.salt = crypto.randomBytes(16).toString('hex');
-    this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+    this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, 'sha1').toString('hex');
 };
 
 schema.methods.validPassword = function (password) {
-    var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+    var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, 'sha1').toString('hex');
     return this.hash === hash;
 };
 
